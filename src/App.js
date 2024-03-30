354174a9e4d70a8c8d0db0dc1c6499a988ad80f1
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-30 15:50:59 +0300

    add  Break Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import DictionaryContainer from './components/DictionaryContainer';
 import WordOfTheDay from './components/WordOfTheDay';
+import BreakComponent from './components/BreakComponent';
 
 function App() {
   return (
     <div className="app-container">
     <DictionaryContainer />
+    <BreakComponent />
     <WordOfTheDay />
   </div>
   );
---
+++ src/components/BreakComponent.js
@@ -0,0 +1,14 @@
+// src/components/BreakComponent.js
+
+import React from 'react';
+import './../css/BreakComponent.css';
+
+const BreakComponent = () => {
+    return (
+        <div className="break-component">
+            <div className="decorative-line"></div>
+        </div>
+    );
+};
+
+export default BreakComponent;
