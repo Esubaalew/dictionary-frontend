c26f6a9b4763c28f4e2ebb2250e0bd446c9f60c2
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-30 14:56:24 +0300

    changed domain

--- src/api/tools.js
+++ src/api/tools.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const domain = 'https://azebness.onrender.com';
+
 export const getEntries = async (word) => {
     try {
-        const response = await axios.get(`http://localhost:8000/britannica/entries/${word}`);
+        const response = await axios.get(`${domain}/britannica/entries/${word}`);
         return response.data.entries;
     } catch (error) {
         console.error('Error fetching entries:', error);
@@ -12,7 +14,7 @@ export const getEntries = async (word) => {
 
 export const getTotalEntries = async (word) => {
     try {
-        const response = await axios.get(`http://localhost:8000/britannica/total_entries/${word}`);
+        const response = await axios.get(`${domain}/britannica/total_entries/${word}`);
         return response.data.total_entries;
     } catch (error) {
         console.error('Error fetching total entries:', error);
@@ -22,7 +24,7 @@ export const getTotalEntries = async (word) => {
 
 export const getWordOfTheDay = async () => {
     try {
-        const response = await axios.get('http://localhost:8000/britannica/word_of_the_day');
+        const response = await axios.get(`${domain}/britannica/word_of_the_day`);
         return response.data.word_of_the_day;
     } catch (error) {
         console.error('Error fetching word of the day:', error);
@@ -32,7 +34,7 @@ export const getWordOfTheDay = async () => {
 
 export const getPartsOfSpeech = async (word) => {
     try {
-        const response = await axios.get(`http://localhost:8000/britannica/speeches/${word}`);
+        const response = await axios.get(`${domain}/britannica/speeches/${word}`);
         return response.data.parts_of_speech;
     } catch (error) {
         console.error('Error fetching parts of speech:', error);
@@ -42,7 +44,7 @@ export const getPartsOfSpeech = async (word) => {
 
 export const getDefinitions = async (word) => {
     try {
-        const response = await axios.get(`http://localhost:8000/britannica/definitions/${word}`);
+        const response = await axios.get(`${domain}/britannica/definitions/${word}`);
         return response.data.definitions;
     } catch (error) {
         console.error('Error fetching definitions:', error);
--- src/components/DictionaryContainer.js
+++ src/components/DictionaryContainer.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { getDefinitions } from '../api/tools';
 import SearchBox from '../components/SearchBox';
 import LoadingSpinner from '../components/LoadingSpinner';
-import './../css/DictionaryContainer.css'; 
+import './../css/DictionaryContainer.css';
 
 const DictionaryContainer = () => {
     const [definitions, setDefinitions] = useState([]);
@@ -23,19 +23,22 @@ const DictionaryContainer = () => {
             <SearchBox onSearch={searchDefinitions} />
             {loading ? (
                 <LoadingSpinner /> // Display loading spinner while waiting for search results
-            ) :(
-            <div>
-                {definitions.map((definition, index) => (
-                    <div key={index}>
-                        <h3>{definition.meaning}</h3>
-                        <ul>
-                            {definition.examples.map((example, index) => (
-                                <li key={index}>{example}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </div>)}
+            ) : (
+                <div>
+                    {definitions.map((definition, index) => (
+                        <div key={index}>
+                            <h3>{definition.meaning}</h3>
+                            <ul>
+                                {definition.examples.map((example, index) => (
+                                    <li key={index}>{example}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                    {/* Display a message if no definitions are found */}
+                    {definitions.length === 0 && <p>No definitions found.</p>}
+                </div>
+            )}
         </div>
     );
 };
