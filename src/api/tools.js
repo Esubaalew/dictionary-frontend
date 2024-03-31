ad01d375af8c76afd552f4059ad7a4e3c308631f
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-31 11:23:50 +0300

    removed addtional endpiont for ease

--- src/api/tools.js
+++ src/api/tools.js
@@ -4,7 +4,7 @@ const domain = 'https://azeb.onrender.com';
 
 export const getEntries = async (word) => {
     try {
-        const response = await axios.get(`${domain}/britannica/entries/${word}`);
+        const response = await axios.get(`${domain}/entries/${word}`);
         return response.data.entries;
     } catch (error) {
         console.error('Error fetching entries:', error);
@@ -14,7 +14,7 @@ export const getEntries = async (word) => {
 
 export const getTotalEntries = async (word) => {
     try {
-        const response = await axios.get(`${domain}/britannica/total_entries/${word}`);
+        const response = await axios.get(`${domain}/total_entries/${word}`);
         return response.data.total_entries;
     } catch (error) {
         console.error('Error fetching total entries:', error);
@@ -24,7 +24,7 @@ export const getTotalEntries = async (word) => {
 
 export const getWordOfTheDay = async () => {
     try {
-        const response = await axios.get(`${domain}/britannica/word_of_the_day`);
+        const response = await axios.get(`${domain}/word_of_the_day`);
         return response.data.word_of_the_day;
     } catch (error) {
         console.error('Error fetching word of the day:', error);
@@ -34,7 +34,7 @@ export const getWordOfTheDay = async () => {
 
 export const getPartsOfSpeech = async (word) => {
     try {
-        const response = await axios.get(`${domain}/britannica/speeches/${word}`);
+        const response = await axios.get(`${domain}/speeches/${word}`);
         return response.data.parts_of_speech;
     } catch (error) {
         console.error('Error fetching parts of speech:', error);
@@ -44,7 +44,7 @@ export const getPartsOfSpeech = async (word) => {
 
 export const getDefinitions = async (word) => {
     try {
-        const response = await axios.get(`${domain}/britannica/definitions/${word}`);
+        const response = await axios.get(`${domain}/definitions/${word}`);
         return response.data.definitions;
     } catch (error) {
         console.error('Error fetching definitions:', error);
